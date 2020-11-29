fb455cf89d38802839506fb5bcdb1e8378f29fa1
drklee3 <derrick@dlee.dev>
2020-11-28 19:26:52 -0800

    feat: Prevent closing stream when deserilizing fails

--- example/src/main.rs
+++ example/src/main.rs
@@ -18,7 +18,7 @@ async fn main() {
     pin_mut!(stream);
 
     while let Some(e) = stream.next().await {
-        let (shard_id, event) = match e {
+        let event = match e {
             Ok(e) => e,
             Err(e) => {
                 tracing::warn!("Error reading event: {}", e);
@@ -27,21 +27,21 @@ async fn main() {
             }
         };
 
-        tokio::spawn(handle_event(shard_id, event));
+        tokio::spawn(handle_event(event));
     }
 }
 
 async fn handle_event(
-    shard_id: u64,
     event: DispatchEvent,
     // http: HttpClient,
 ) -> Result<()> {
     let event_name = event.kind().name().unwrap();
 
-    tracing::info!("Received event on shard {}: {}", shard_id, event_name);
+    tracing::debug!("Received event: {}", event_name);
 
     match event {
-        DispatchEvent::MessageCreate(msg) if msg.content == "!ping" => {
+        DispatchEvent::MessageCreate(msg) => {
+            tracing::info!("Received message: {}#{}: {}", msg.author.name, msg.author.discriminator, msg.content);
             // http.create_message(msg.channel_id).content("Pong!")?.await?;
         }
         // Other events here...
--- processor/src/events.rs
+++ processor/src/events.rs
@@ -9,14 +9,13 @@ use twilight_model::gateway::event::gateway::GatewayEventDeserializer;
 
 use crate::error::Result;
 
-pub fn get_events(redis_addr: String) -> impl Stream<Item = Result<(u64, DispatchEvent)>> {
+pub fn get_events(redis_addr: String) -> impl Stream<Item = Result<DispatchEvent>> {
     try_stream! {
         let mut conn = Connection::connect(redis_addr).await?;
 
         loop {
             if let Some((_list, event)) = conn.blpop(&["events"], 0).await? {
                 let event_str = String::from_utf8_lossy(&event);
-                tracing::info!("Event json string: {}", event_str);
 
                 let de = match GatewayEventDeserializer::from_json(&event_str) {
                     Some(d) => d,
@@ -27,23 +26,24 @@ pub fn get_events(redis_addr: String) -> impl Stream<Item = Result<(u64, Dispatc
                 };
 
                 let mut json_deserializer = Deserializer::from_str(&event_str);
-                let gateway_event = de.deserialize(&mut json_deserializer)?;
-
-                let (shard_id, dispatch_event) = match gateway_event {
-                    GatewayEvent::Dispatch(shard_id, dispatch_event) => {
-                        tracing::info!("blpop: {:?}", dispatch_event.kind());
-
-                        (shard_id, dispatch_event)
-                    }
-                    // Heartbeat(u64), HeartbeatAck, Hello(u64), InvalidateSession(bool), Reconnect,
-                    _ => {
+                let gateway_event = match de.deserialize(&mut json_deserializer) {
+                    Ok(event) => event,
+                    Err(e) => {
+                        tracing::warn!("Failed to deserialize event: {}", e);
                         continue;
                     }
                 };
 
+                let dispatch_event = match gateway_event {
+                    // (sequence number, event) don't need seq
+                    GatewayEvent::Dispatch(_, dispatch_event) => dispatch_event,
+                    // Heartbeat(u64), HeartbeatAck, Hello(u64), InvalidateSession(bool), Reconnect,
+                    _ => continue,
+                };
+
                 // Not sure if I should return a Box<DispatchEvent> or move it
                 // back to the stack like this
-                yield (shard_id, *dispatch_event);
+                yield *dispatch_event;
             }
         }
     }
